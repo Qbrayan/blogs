fa0aa3be460e18c9f4d25eb9d8e1947bee417922
Brian Kariuki <bkariuki@CT-BKARIUKI.local>
2017-03-02 18:45:13 +0300

    laravel start

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+            <!-- RECENT POSTS -->
+            <div class="row recent_posts" data-appear-top-offset="-200" data-animated="fadeInUp">
+
+                <div class="col-sm-4 ">
+                    <div >
+                        <div >
+                            <img src="http://www.carbazaar.us/wp-content/uploads/2015/10/IMG_0914-460x320.jpg" alt="" />
+                            <a class="link" href="#" target="_blank"></a>
+                        </div>
+                        <div >
+                            <a class="title" href="#" target="_blank" >Guide to Selecting best cars</a>
+                            <ul class="post_item_inf">
+                                <li><a href="javascript:void(0);" >Cytonn bazaar</a> |</li>
+                                <li><a href="javascript:void(0);" >Motor vehicle guide</a> |</li>
+                                <li><a href="javascript:void(0);" >15 Likes</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-sm-4 ">
+                    <div >
+                        <div >
+                            <img src="http://www.dev.punjabilinks.com/upload/bus_img3886the-new-bazaar-car-4799d.jpg" alt="" />
+                            <a class="link" href="#" target="_blank" ></a>
+                        </div>
+                        <div >
+                            <a class="title" href="#" target="_blank" >Precision and elegance</a>
+                            <ul class="post_item_inf">
+                                <li><a href="javascript:void(0);" >Cytonn bazaar</a> |</li>
+                                <li><a href="javascript:void(0);" >Smart ride</a> |</li>
+                                <li><a href="javascript:void(0);" >10 Likes</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4 ">
+                    <div >
+                        <div >
+                            <img src="http://www.carkhabri.com/Gallery/volvo/volvo-s60/exterior/large/8.jpg" alt="" />
+                            <a class="link" href="#" target="_blank"></a>
+                        </div>
+                        <div >
+                            <a class="title" href="#" target="_blank">Motor vehicles</a>
+                            <ul class="post_item_inf">
+                                <li><a href="javascript:void(0);" >Cytonn bazaar</a> |</li>
+                                <li><a href="javascript:void(0);" >Our partners</a> |</li>
+                                <li><a href="javascript:void(0);" >20 Likes</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+@endsection
